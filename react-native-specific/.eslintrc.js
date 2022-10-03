f3d964d3a69909098696b32af792ab05850a0071
Tarun Soni <soni.tarun26@gmail.com>
2022-10-04 00:53:42 +0530

    added rn eslint

---
+++ react-native-specific/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  extends: '@react-native-community',
+  rules: {
+    semi: 0,
+    'eslint-comments/no-unlimited-disable': 0,
+    'react/jsx-closing-bracket-location': 0,
+    'react/jsx-props-no-spreading': 0,
+    'react/jsx-curly-new-line': 0,
+    'no-param-reassign': 0,
+    'react-native/no-inline-styles': 0,
+    'react-native/no-unused-styles': 1,
+    'react-native/split-platform-components': 2,
+    'no-console': 1,
+    'react-native/no-color-literals': 1,
+    'react-native/no-raw-text': 2,
+    'eslint-comments/no-unused-disable': 0,
+    curly: 0,
+  },
+}
